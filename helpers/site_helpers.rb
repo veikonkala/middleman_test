190ad13bad51bdc197bbb2c842ab06c17d57a3b8
Timo Karsisto <tkarsist@timsu.local>
2013-02-12 19:59:31 +0200

    added the project

---
+++ helpers/site_helpers.rb
@@ -0,0 +1,31 @@
+module SiteHelpers
+
+  def page_title
+    title = "Set your site title in /helpers/site_helpers.rb"
+    if data.page.title
+      title << " | " + data.page.title
+    end
+    title
+  end
+  
+  def page_description
+    if data.page.description
+      description = data.page.description
+    else
+      description = "Set your site description in /helpers/site_helpers.rb"
+    end
+    description
+  end
+  
+  def embed_content(contentID,default="hahaha")
+    contentID=local_assigns[:contentID] || true
+    if contentID
+      content= default
+    else
+      content= contentID
+    end
+    content
+  end
+  
+
+end
---
+++ readme.txt
@@ -0,0 +1,5 @@
+bundle exec middleman server
+
+vaatii: middleman, compass, haml ja sass
+
+localhost:4567/test.html
